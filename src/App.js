979637087351ccf5618fcf72c76fc1a675991bef
Damian Barera <team@platzi.com>
2023-02-03 18:39:58 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './assets/main.css';
 
-function App() {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full max-w-sm mx-auto mt-10 text-center">
+      <h1 className="block font-bold mb-2 text-gray-1500"> Login Challenge </h1>
+      <h2 className="block font-bold mb-2 text-gray-1000"> By Damián Barera </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block font-bold mb-2 text-gray-400" htmlFor="username">
+            Nombre de usuario
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block font-bold mb-2 text-gray-400" htmlFor="password">
+            Contraseña
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="text-black hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Iniciar sesión
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+export default Login;
+
